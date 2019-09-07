c334c8982fc60626e5d24787e14438bea5692aac
jinniananswer <38103152@qq.com>
2019-09-08 01:35:25 +0800

    增加深色和红色的弹出层样式定义

--- src/main/resources/static/js/extend/ajax.js
+++ src/main/resources/static/js/extend/ajax.js
@@ -20,39 +20,55 @@ layui.define(['layer', 'jquery'], function(exports){
 
         execute : function(url, param, type, successFunc, errFunc, async) {
             var successMethod = null;
-            if(successFunc == null || typeof(successFunc) == "undefined" || typeof(successFunc) != "function") {
-                successMethod = function(data) {
-                    layer.closeAll('loading');
 
+            successMethod = function(data) {
+                var code = data.code;
+                layer.closeAll('loading');
+                if (code != '0') {
+                    //后端有报错
+                    layer.open({
+                        type : 100,
+                        skin : 'layui-layer-red', //样式类名
+                        closeBtn : 1, //显示关闭按钮
+                        anim : 2,
+                        shadeClose: false, //开启遮罩关闭
+                        shade : [0.5, '#fff'],
+                        content : data.message
+                    });
+                } else if (successFunc == null || typeof(successFunc) == "undefined" || typeof(successFunc) != "function") {
                     layer.confirm('操作成功，点击确定按钮刷新本页面，点击关闭按钮关闭本界面？', {
                         btn : ['确定','关闭'],
                         closeBtn : 0,
                         icon : 6,
                         title : '提示信息',
+                        shade : [0.5, '#fff'],
                         skin : 'layui-layer-admin layui-anim'
                     }, function(){
                         document.location.reload();
                     }, function(){
                         top.layui.admin.closeThisTabs();
                     });
-                }
-            }
-            else {
-
-                successMethod = function(data) {
-                    layer.closeAll('loading');
+                } else {
                     successFunc(data);
                 }
             }
 
-
             if(errFunc == null || typeof(errFunc) == "undefined" || typeof(errFunc) != "function") {
                 errFunc = function(data){
                     layer.closeAll('loading');
-                    layer.alert("对不起，偶们的系统出错了，55555555555555,亲，赶紧联系管理员报告功能问题吧");
+                    layer.open({
+                        type : 1,
+                        skin : 'layui-layer-red', //样式类名
+                        closeBtn: 1, //不显示关闭按钮
+                        anim : 2,
+                        shadeClose : true, //开启遮罩关闭
+                        content : "对不起，55555555555，偶们的系统出错了，赶紧联系管理员吧"
+                    });
                 };
             }
-            layer.load();
+            layer.load(1,{
+                shade: [0.3,'#fff'] //0.1透明度的白色背景
+            });
             $.ajax(
                 {
                     url:url,
@@ -64,7 +80,8 @@ layui.define(['layer', 'jquery'], function(exports){
                     success: successMethod,
                     error: errFunc
                 }
-            )
+            );
+
         }
     };
     exports('ajax', obj);
