d704cad0005d2f2e29bcd97a408dc4cda9cb52c9
jinniananswer <38103152@qq.com>
2019-09-08 19:54:49 +0800

    修改跳转

--- src/main/java/com/microtomato/hirun/modules/user/controller/UserController.java
+++ src/main/java/com/microtomato/hirun/modules/user/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
-@RequestMapping("/api/user/user")
+@RequestMapping("/api/user/")
 public class UserController {
 
     @Autowired
