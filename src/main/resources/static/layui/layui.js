c4241dea4b4fabf91d49724d4c73fdcb997aa2e0
jinniananswer <38103152@qq.com>
2019-09-13 23:21:29 +0800

    更新layui框架版本

--- src/main/resources/static/layui/layui.js
+++ src/main/resources/static/layui/layui.js
@@ -1,7 +1,7 @@
 /*!
 
- @Title: Layui
- @Description：经典模块化前端框架
+ @Title: layui
+ @Description：经典模块化前端 UI 框架
  @Site: www.layui.com
  @Author: 贤心
  @License：MIT
@@ -19,7 +19,7 @@
   }
 
   ,Layui = function(){
-    this.v = '2.5.4'; //版本号
+    this.v = '2.5.5'; //版本号
   }
 
   //获取layui所在目录
@@ -508,9 +508,9 @@
   Layui.prototype.event = Layui.event = function(modName, events, params, fn){
     var that = this
     ,result = null
-    ,filter = events.match(/\((.*)\)$/)||[] //提取事件过滤器字符结构，如：select(demo)
+    ,filter = events.match(/\((.*)\)$/)||[] //提取事件过滤器字符结构，如：select(xxx)
     ,eventName = (modName + '.'+ events).replace(filter[0], '') //获取事件名称，如：form.select
-    ,filterName = filter[1] || '' //获取过滤器名称,，如：demo
+    ,filterName = filter[1] || '' //获取过滤器名称,，如：xxx
     ,callback = function(_, item){
       var res = item && item.call(that, params);
       res === false && result === null && (result = false);
