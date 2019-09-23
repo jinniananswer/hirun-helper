a133e62e98c64ea2f523071970cc33e7f1296469
liuhui <lh936679052@163.com>
2019-09-23 19:20:20 +0800

    新增密码修改方法

--- src/main/resources/static/js/modules/system/change.staffpassword.js
+++ src/main/resources/static/js/modules/system/change.staffpassword.js
@@ -1,20 +1,20 @@
-layui.define(['ajax', 'form', 'layer'],function(exports){
+layui.define(['ajax', 'form', 'layer'], function (exports) {
     var $ = layui.$;
     var form = layui.form;
     var changestaffpwd = {
-        init : function() {
+        init: function () {
 
             form.verify({
-                passwordlength :function (password) {
-                    if(password.length!=6){
+                passwordlength: function (password) {
+                    if (password.length != 6) {
                         return '密码长度必须为6';
                     }
                 },
-                });
+            });
 
-            form.on('submit(changeStaffPassword)', function(data){
-                var filed=data.field;
-                if(filed.password !== filed.repassword){
+            form.on('submit(changeStaffPassword)', function (data) {
+                var filed = data.field;
+                if (filed.password !== filed.repassword) {
                     return layer.msg("两次密码输入不一致",);
                 }
 
@@ -24,13 +24,13 @@ layui.define(['ajax', 'form', 'layer'],function(exports){
 
         },
 
-        changestaffpwd : function(formData) {
-            layui.ajax.post('/api/user/user/changeStaffPassword', formData, function(data) {
-                layer.confirm('密码修改成功。点击确定返回页面。',{
+        changestaffpwd: function (formData) {
+            layui.ajax.post('/api/user/user/changeStaffPassword', formData, function (data) {
+                layer.confirm('密码修改成功。点击确定返回页面。', {
                     btn: ['确定']
-                }, function(){
+                }, function () {
                     document.location.reload();
-                }, );
+                },);
             });
         }
     };
