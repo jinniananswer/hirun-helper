5412a07b858dfbe30b08a6cc9059bf4b748b5697
liuhui <lh936679052@163.com>
2019-09-23 18:45:21 +0800

    新增密码修改方法

--- src/main/java/com/microtomato/hirun/modules/user/service/IUserService.java
+++ src/main/java/com/microtomato/hirun/modules/user/service/IUserService.java
@@ -14,4 +14,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IUserService extends IService<User> {
 
     User login(String username, String password);
+
+    boolean changeStaffPassword(Integer userId,String oldPassword,String newPassword);
 }
