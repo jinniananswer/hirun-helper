d5807b32bc8262a263e6c78c884b552aa31a1bbb
zhoulin2 <zhoulin2@asiainfo.com>
2019-09-24 15:33:49 +0800

    [修改] 补充多表关联查询的几种示例

---
+++ src/main/resources/static/js/modules/system/change.staffpassword.js
@@ -0,0 +1,38 @@
+layui.define(['ajax', 'form', 'layer'], function (exports) {
+    var $ = layui.$;
+    var form = layui.form;
+    var changestaffpwd = {
+        init: function () {
+
+            form.verify({
+                passwordlength: function (password) {
+                    if (password.length != 6) {
+                        return '密码长度必须为6';
+                    }
+                },
+            });
+
+            form.on('submit(changeStaffPassword)', function (data) {
+                var filed = data.field;
+                if (filed.password !== filed.repassword) {
+                    return layer.msg("两次密码输入不一致");
+                }
+
+                layui.changestaffpwd.changestaffpwd(data.field);
+
+            });
+
+        },
+
+        changestaffpwd: function (formData) {
+            layui.ajax.post('/api/user/user/changeStaffPassword', formData, function (data) {
+                layer.confirm('密码修改成功。点击确定返回页面。', {
+                    btn: ['确定']
+                }, function () {
+                    document.location.reload();
+                });
+            });
+        }
+    };
+    exports('changestaffpwd', changestaffpwd);
+});
