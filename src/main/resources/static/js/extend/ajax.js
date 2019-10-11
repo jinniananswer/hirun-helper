8a2ebd27906189f8c263c2da5ba18c818d945462
jinniananswer <38103152@qq.com>
2019-10-12 01:37:43 +0500

    员工档案新增代码提交

--- src/main/resources/static/js/extend/ajax.js
+++ src/main/resources/static/js/extend/ajax.js
@@ -3,39 +3,31 @@ layui.define(['layer', 'jquery'], function(exports){
     var layer = layui.layer;
     var obj = {
         get : function(url, param, successFunc, errFunc, needLoad) {
-            if (needLoad == null || typeof(needLoad) == "undefined") {
-                needLoad = true;
-            }
-            this.execute(url, param, 'GET', successFunc, errFunc, false, needLoad);
-        },
-
-        post : function(url, param, successFunc, errFunc, needLoad) {
-            if (needLoad == null || typeof(needLoad) == "undefined") {
-                needLoad = true;
-            }
-            this.execute(url, param, 'POST', successFunc, errFunc, false, needLoad);
-        },
-
-        getAsync : function(url, param ,successFunc, errFunc, needLoad) {
-            if (needLoad == null || typeof(needLoad) == "undefined") {
-                needLoad = true;
-            }
             this.execute(url, param, 'GET', successFunc, errFunc, true, needLoad);
         },
 
-        postAsync : function(url, param, successFunc, errFunc, needLoad) {
-            if (needLoad == null || typeof(needLoad) == "undefined") {
-                needLoad = true;
-            }
+        post : function(url, param, successFunc, errFunc, needLoad) {
             this.execute(url, param, 'POST', successFunc, errFunc, true, needLoad);
         },
 
+        getSync : function(url, param ,successFunc, errFunc, needLoad) {
+            //注意，同步方式load不起作用
+            this.execute(url, param, 'GET', successFunc, errFunc, false, needLoad);
+        },
+
+        postSync : function(url, param, successFunc, errFunc, needLoad) {
+            //注意，同步方式load不起作用
+            this.execute(url, param, 'POST', successFunc, errFunc, false, needLoad);
+        },
+
         execute : function(url, param, type, successFunc, errFunc, async, needLoad) {
+            if (needLoad == null || typeof(needLoad) == "undefined") {
+                needLoad = true;
+            }
             var successMethod = null;
 
             successMethod = function(data) {
                 var code = data.code;
-                layer.closeAll('loading');
                 if (code != '0') {
                     //后端有报错
                     layer.msg(data.code + ": " + data.message,{
@@ -61,17 +53,19 @@ layui.define(['layer', 'jquery'], function(exports){
                 } else {
                     successFunc(data);
                 }
+                layer.closeAll('loading');
             }
 
             if(errFunc == null || typeof(errFunc) == "undefined" || typeof(errFunc) != "function") {
                 errFunc = function(data){
                     layer.closeAll('loading');
-                    layer.alert('对不起，55555555555，偶们的系统出错了，赶紧联系管理员吧', {
+                    layer.alert('对不起，55555555555，偶们的系统出错了，'+data.statusText+'，赶紧联系管理员吧', {
                         skin: 'layui-layer-molv'
                         ,closeBtn: 0
                     });
                 };
             }
+
             if (needLoad) {
                 layer.load(1,{
                     shade: [0.3,'#fff'] //0.1透明度的白色背景
