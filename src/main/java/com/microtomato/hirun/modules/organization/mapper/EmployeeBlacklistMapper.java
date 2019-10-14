a7d1fbfe5eef4c57121d10b2565561c289177ff0
liuhui <lh936679052@163.com>
2019-10-14 21:37:35 +0800

    【修改】修改员工黑名单映射类

--- src/main/java/com/microtomato/hirun/modules/organization/mapper/EmployeeBlacklistMapper.java
+++ src/main/java/com/microtomato/hirun/modules/organization/mapper/EmployeeBlacklistMapper.java
@@ -1,5 +1,6 @@
 package com.microtomato.hirun.modules.organization.mapper;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.microtomato.hirun.modules.organization.entity.po.EmployeeBlacklist;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
  * @author liuhui
  * @since 2019-10-14
  */
+@DS("ins")
 @Mapper
 public interface EmployeeBlacklistMapper extends BaseMapper<EmployeeBlacklist> {
 
