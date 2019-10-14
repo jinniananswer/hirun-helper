8aa457c37202227bc1a79d9ef02c2ed1d8789717
liuhui <lh936679052@163.com>
2019-10-14 21:35:04 +0800

    【新增】新增员工黑名单modules

---
+++ src/main/java/com/microtomato/hirun/modules/organization/controller/EmployeeBlacklistController.java
@@ -0,0 +1,31 @@
+package com.microtomato.hirun.modules.organization.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+
+import com.microtomato.hirun.modules.organization.service.IEmployeeBlacklistService;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author liuhui
+ * @since 2019-10-14
+ */
+@RestController
+@Slf4j
+@RequestMapping("/api/organization/employee-blacklist")
+public class EmployeeBlacklistController {
+
+    @Autowired
+    private IEmployeeBlacklistService employeeBlacklistServiceImpl;
+
+
+
+}
---
+++ src/main/java/com/microtomato/hirun/modules/organization/mapper/EmployeeBlacklistMapper.java
@@ -0,0 +1,18 @@
+package com.microtomato.hirun.modules.organization.mapper;
+
+import com.microtomato.hirun.modules.organization.entity.po.EmployeeBlacklist;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author liuhui
+ * @since 2019-10-14
+ */
+@Mapper
+public interface EmployeeBlacklistMapper extends BaseMapper<EmployeeBlacklist> {
+
+}
