28bce8ae553c9bb5edc68ace3a084ff029168314
zhoulin2 <zhoulin2@asiainfo.com>
2019-10-15 22:57:39 +0800

    [新增] 转换器: Json 对象里的 String 自动转换成 LocalDateTime

--- src/main/java/com/microtomato/hirun/modules/system/controller/FuncController.java
+++ src/main/java/com/microtomato/hirun/modules/system/controller/FuncController.java
@@ -1,13 +1,9 @@
 package com.microtomato.hirun.modules.system.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import lombok.extern.slf4j.Slf4j;
-
 import com.microtomato.hirun.modules.system.service.IFuncService;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,7 +21,5 @@ public class FuncController {
 
     @Autowired
     private IFuncService funcServiceImpl;
-
-
 
 }
