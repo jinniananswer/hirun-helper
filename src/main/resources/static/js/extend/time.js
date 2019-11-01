d52088852b920c8122bcfa39624c45c85cb83d4d
jinniananswer <38103152@qq.com>
2019-11-02 02:29:01 +0500

    【修改】添加加减月份函数

--- src/main/resources/static/js/extend/time.js
+++ src/main/resources/static/js/extend/time.js
@@ -1,4 +1,4 @@
-layui.define(null, function(exports){
+layui.define([], function(exports){
     var $ = layui.$;
     var obj = {
         getYearDiff : function(startDate, endDate) {
@@ -34,6 +34,43 @@ layui.define(null, function(exports){
                 return true;
             }
             return false;
+        },
+
+        format : function(date) {
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            month = month.length > 1 ? month : "0"+month;
+
+            var day = date.getDate();
+            day = day.length > 1 ? day : "0"+day;
+
+            return year + "-" + month + "-" + day;
+        },
+
+        addMonth : function(date, num) {
+            num = parseInt(num);
+            var sDate = new Date(date);
+
+            var sYear = sDate.getFullYear();
+            var sMonth = sDate.getMonth() + 1;
+            var sDay = sDate.getDate();
+
+            var eYear = sYear;
+            var eMonth = sMonth + num;
+            var eDay = sDay;
+            while (eMonth > 12) {
+                eYear++;
+                eMonth -= 12;
+            }
+
+            var eDate = new Date(eYear, eMonth - 1, eDay);
+
+            while (eDate.getMonth() != eMonth - 1) {
+                eDay--;
+                eDate = new Date(eYear, eMonth - 1, eDay);
+            }
+
+            return this.format(eDate);
         }
     };
     exports('time', obj);
