f7a3aa1aa5f866294ae6aa3aba8f2b02458e4848
zhoulin2 <zhoulin2@asiainfo.com>
2019-11-15 09:44:42 +0800

    [新增] 前端应用 和 后端任务 分开部署支持。

--- src/main/java/com/microtomato/hirun/HirunHelperApplication.java
+++ src/main/java/com/microtomato/hirun/HirunHelperApplication.java
@@ -4,7 +4,6 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 注解 ServletComponentScan 用于扫描过滤器 Filter 并自动装配，Filter 类需标注 @WebFilter 注解。
@@ -14,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @ServletComponentScan
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
-@EnableScheduling
 public class HirunHelperApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/microtomato/hirun/framework/config/SchedulingConfig.java
@@ -0,0 +1,23 @@
+package com.microtomato.hirun.framework.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 后台任务 和 前端应用 应分开部署:<p>
+ * 前端应用不启用 @EnableScheduling
+ * 后台任务需启用 @EnableScheduling
+ * <p>
+ * 启用 @EnableScheduling 的方法两种人选其一：
+ * 1. application.yml 添加 scheduling.enabled: true
+ * 2. 启动命令行添加 --scheduling.enabled=true
+ *
+ * @author Steven
+ * @date 2019-11-15
+ */
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(prefix = "scheduling", name = "enabled", havingValue = "true")
+public class SchedulingConfig {
+}
