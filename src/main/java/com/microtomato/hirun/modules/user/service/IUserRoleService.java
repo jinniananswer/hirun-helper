828a7c4a6c40c0dce334bbdc343a1a7ba303189d
zhoulin2 <zhoulin2@asiainfo.com>
2019-11-16 20:14:58 +0800

    [新增] 根据给定的 用户Id、部门Id、以及岗位，创建对应的 ins_user_role 关联数据。

--- src/main/java/com/microtomato/hirun/modules/user/service/IUserRoleService.java
+++ src/main/java/com/microtomato/hirun/modules/user/service/IUserRoleService.java
@@ -13,4 +13,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IUserRoleService extends IService<UserRole> {
 
+    /**
+     * 根据给定的 用户Id、部门Id、以及岗位，创建对应的 ins_user_role 关联数据。
+     *
+     * @param userId 用户Id
+     * @param orgId 部门Id，对应 ins_org.org_id
+     * @param jobRole 岗位编码，对应 code_value, select * from sys_static_data t where t.code_type='JOB_ROLE';
+     */
+    void createUserRole(Long userId, Long orgId, String jobRole);
 }
--- src/main/java/com/microtomato/hirun/modules/user/service/impl/UserRoleServiceImpl.java
+++ src/main/java/com/microtomato/hirun/modules/user/service/impl/UserRoleServiceImpl.java
@@ -19,4 +19,15 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements IUserRoleService {
 
+    /**
+     * 根据给定的 用户Id、部门Id、以及岗位，创建对应的 ins_user_role 关联数据。
+     *
+     * @param userId 用户Id
+     * @param orgId 部门Id，对应 ins_org.org_id
+     * @param jobRole 岗位编码，对应 code_value, select * from sys_static_data t where t.code_type='JOB_ROLE';
+     */
+    @Override
+    public void createUserRole(Long userId, Long orgId, String jobRole) {
+        // TODO：根据映射表的配置，对应上角色数据。
+    }
 }
