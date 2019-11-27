08585ffab308e848e44df0d30768d73fc5169892
zhoulin2 <zhoulin2@asiainfo.com>
2019-11-27 16:11:49 +0800

    [新增] 公告编辑页面。

--- src/main/java/com/microtomato/hirun/modules/system/controller/NotifyController.java
+++ src/main/java/com/microtomato/hirun/modules/system/controller/NotifyController.java
@@ -3,10 +3,10 @@ package com.microtomato.hirun.modules.system.controller;
 import com.microtomato.hirun.framework.annotation.RestResult;
 import com.microtomato.hirun.modules.system.service.INotifyService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -24,13 +24,21 @@ public class NotifyController {
     @Autowired
     private INotifyService notifyServiceImpl;
 
-    @GetMapping("/testSendAnnounce")
+    @PostMapping("/sendAnnounce")
     @RestResult
-    public void sendAnnounce() {
-        notifyServiceImpl.sendAnnounce("公告：上线在即，请各位小伙伴快马加鞭。");
+    public void sendAnnounce(@RequestBody String content) {
+        log.error(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        log.error(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        log.error(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        log.error(content);
+
+        content = StringEscapeUtils.unescapeJava(content);
+        content = StringUtils.strip(content, "\"");
+        log.error(content);
+        notifyServiceImpl.sendAnnounce(content);
     }
 
-    @GetMapping("/testSendMessage")
+    @GetMapping("/sendMessage")
     @RestResult
     public void sendMessage() {
         notifyServiceImpl.sendMessage(1473L, "这是一条测试消息");
---
+++ src/main/resources/static/js/modules/system/announce-create.js
@@ -0,0 +1,38 @@
+layui.use(['ajax', 'layer', 'layedit'], function () {
+    let $ = layui.$;
+    let layedit = layui.layedit;
+    let layer = layui.layer;
+
+    // 构建一个默认的编辑器
+    let index = layedit.build('LAY_announce-textarea', {
+        tool: ['strong', 'italic', 'underline', 'del', '|', 'left', 'center', 'right', '|',  'face', 'link', 'unlink'],
+        height: 100
+    });
+
+    // 编辑器外部操作
+    let active = {
+        createAnnounce: function () {
+            let content = layedit.getContent(index); // 获取编辑器内容
+            $.ajax({
+                type: "post",
+                url: "api/system/notify/sendAnnounce",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(content),
+                success: function (obj) {
+                    layer.msg("操作成功！");
+                },
+                error: function (obj) {
+                    layer.alert("操作失败！");
+                }
+            });
+        },
+    };
+
+    $('.site-demo-layedit').on('click', function () {
+        let type = $(this).data('type');
+        active[type] ? active[type].call(this) : '';
+    });
+
+
+});
