0bd57a79675305c62a4b5b82af22a052b4da0e05
zhoulin2 <zhoulin2@asiainfo.com>
2019-12-02 11:07:04 +0800

    [调试] 新系统打开老系统

--- src/main/java/com/microtomato/hirun/modules/system/controller/MenuController.java
+++ src/main/java/com/microtomato/hirun/modules/system/controller/MenuController.java
@@ -68,7 +68,7 @@ public class MenuController {
         // 查询所有菜单集合
         Map<Long, Menu> menuMap = menuServiceImpl.listAllMenus();
 
-        convert(menuMap);
+        menuMap = convert(menuMap);
 
         // 根据权限进行过滤
         Set<String> menuUrls = new HashSet<>();
