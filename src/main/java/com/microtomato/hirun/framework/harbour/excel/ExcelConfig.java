0ab409940f948765cced9cb636603d46ceb617e3
zhoulin2 <zhoulin2@asiainfo.com>
2019-12-03 14:31:08 +0800

    [新增] 完善文档之《支持 Excel 基于模板的导出》

--- src/main/java/com/microtomato/hirun/framework/harbour/excel/ExcelConfig.java
+++ src/main/java/com/microtomato/hirun/framework/harbour/excel/ExcelConfig.java
@@ -3,20 +3,41 @@ package com.microtomato.hirun.framework.harbour.excel;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * 配置类
+ *
  * @author Steven
  * @date 2019-12-03
  */
 @Data
 @Builder
 public class ExcelConfig {
+
+    /**
+     * 导出文件名
+     */
     private String fileName;
+
+    /**
+     * 模板文件名，模板统一存放在：resources/excel-templates 目录下
+     */
     private String templateFileName;
+
+    /**
+     * 填充模板的数据，扁平结构
+     */
     private Map<String, Object> fillMap;
+
+    /**
+     * 模板的 sheet 页下标，从 0 开始。
+     */
     private List<Integer> sheet;
-    private List<List<?>> lists = new ArrayList();
+
+    /**
+     * 填充模板的数据，列表结果
+     */
+    private List<List<?>> lists;
 }
