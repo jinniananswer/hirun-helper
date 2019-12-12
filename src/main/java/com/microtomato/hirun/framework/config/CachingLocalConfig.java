8051b8b999894c2dbf083bf456c431e966fa3a9b
zhoulin2 <zhoulin2@asiainfo.com>
2019-12-12 11:23:35 +0800

    [新增] 本地缓存、Redis缓存可选切换。

--- src/main/java/com/microtomato/hirun/framework/config/CachingLocalConfig.java
+++ src/main/java/com/microtomato/hirun/framework/config/CachingLocalConfig.java
@@ -1,5 +1,7 @@
 package com.microtomato.hirun.framework.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
@@ -7,18 +9,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 缓存配置
+ * 本地缓存配置
  *
  * @author Steven
  * @date 2019-05-04
  */
+@Slf4j
 @Configuration
 @EnableCaching
-public class CachingConfig {
+@ConditionalOnProperty(prefix = "caching", name = "mode", havingValue = "local")
+public class CachingLocalConfig {
 
+    /**
+     * 基于 java.util.concurrent.ConcurrentHashMap 的简易缓存管理器，一般用于开发。
+     *
+     * @return
+     */
     @Bean
     public CacheManager cacheManager() {
-        // 基于 java.util.concurrent.ConcurrentHashMap 的简易缓存管理器。
+        log.info("CacheManager：ConcurrentMapCacheManager");
         return new ConcurrentMapCacheManager();
     }
 
