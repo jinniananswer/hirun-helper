698e99af19c53a6398ee422cebab9f80a6e675d1
liuhui <lh936679052@163.com>
2019-12-19 23:34:19 +0800

    【新增】新增数据权限常量类

---
+++ src/main/java/com/microtomato/hirun/modules/system/entity/consts/FuncConst.java
@@ -0,0 +1,19 @@
+package com.microtomato.hirun.modules.system.entity.consts;
+
+/**
+ * @program: hirun-helper
+ * @description: 数据权限常量对象类
+ * @author: liuhui
+ * @create:
+ **/
+public class FuncConst {
+
+    public static final String ALL_ORG = "ALL_ORG";
+
+    public static final String ALL_CITY = "ALL_CITY";
+
+    public static final String ALL_SHOP = "ALL_SHOP";
+
+    public static final String ALL_WOODEN = "ALL_WOODEN";
+
+}
