771b4c8a66d0d59a7525b5284515fd798fb8ebbc
liuhui <lh936679052@163.com>
2019-12-19 23:36:53 +0800

    【修改】新增枚举说明

--- src/main/java/com/microtomato/hirun/modules/system/entity/consts/FuncConst.java
+++ src/main/java/com/microtomato/hirun/modules/system/entity/consts/FuncConst.java
@@ -7,13 +7,21 @@ package com.microtomato.hirun.modules.system.entity.consts;
  * @create:
  **/
 public class FuncConst {
-
+    /**
+     * 所有部门权限
+     */
     public static final String ALL_ORG = "ALL_ORG";
-
+    /**
+     * 家装事业部权限
+     */
     public static final String ALL_CITY = "ALL_CITY";
-
+    /**
+     * 家装事业部分公司权限
+     */
     public static final String ALL_SHOP = "ALL_SHOP";
-
+    /**
+     * 木制事业部权限
+     */
     public static final String ALL_WOODEN = "ALL_WOODEN";
 
 }
