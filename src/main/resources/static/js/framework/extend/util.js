f32747c4e13cd74f96a824cce6c5922fd769fa68
jinniananswer <38103152@qq.com>
2020-01-30 18:31:08 +0800

    【新增】新增vue-router,util组件

---
+++ src/main/resources/static/js/framework/extend/util.js
@@ -0,0 +1,9 @@
+define([], function(){
+    let util = {
+        getRequestParam: function(name) {
+            return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null;
+        }
+    }
+
+    return util;
+})
--- src/main/resources/static/js/framework/requirejs/require.config.js
+++ src/main/resources/static/js/framework/requirejs/require.config.js
@@ -6,10 +6,12 @@ require.config({
     },
     paths: {
         'vue': 'static/js/framework/vue/vue.min',
+        'vue-router': 'static/js/framework/vue/vue-router',
         'ELEMENT': 'static/element-ui/js/index',
         'axios': 'static/js/framework/axios/axios.min',
         'ajax': 'static/js/framework/extend/ajax',
-        'vueselect': 'static/js/framework/extend/select'
+        'vueselect': 'static/js/framework/extend/select',
+        'util': 'static/js/framework/extend/util'
     },
     shim: {
         'ELEMENT': {
@@ -18,6 +20,7 @@ require.config({
     }
 });
 
-require(['vue', 'ELEMENT'], function(Vue, element){
+require(['vue', 'ELEMENT', 'vue-router'], function(Vue, element, VueRouter){
     element.install(Vue);
+    Vue.use(VueRouter);
 });
