67cebad2365baa25e6dd4b42f5183efc6b10d228
jinniananswer <38103152@qq.com>
2020-02-05 01:50:00 +0800

    【新增】新增根据角色选择员工组件

--- src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderBaseController.java
+++ src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderBaseController.java
@@ -1,10 +1,14 @@
 package com.microtomato.hirun.modules.bss.order.controller;
 
 import com.microtomato.hirun.framework.annotation.RestResult;
+import com.microtomato.hirun.framework.security.UserContext;
+import com.microtomato.hirun.framework.util.WebContextUtils;
 import com.microtomato.hirun.modules.bss.order.entity.dto.OrderInfoDTO;
 import com.microtomato.hirun.modules.bss.order.entity.dto.OrderWorkerDTO;
 import com.microtomato.hirun.modules.bss.order.service.IOrderBaseService;
 import com.microtomato.hirun.modules.bss.order.service.IOrderDomainService;
+import com.microtomato.hirun.modules.organization.entity.dto.SimpleEmployeeDTO;
+import com.microtomato.hirun.modules.organization.service.IEmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +36,9 @@ public class OrderBaseController {
     @Autowired
     private IOrderDomainService orderDomainService;
 
+    @Autowired
+    private IEmployeeService employeeService;
+
     @GetMapping("/getOrderInfo")
     @RestResult
     public OrderInfoDTO getOrderInfo(Long orderId) {
@@ -42,5 +49,13 @@ public class OrderBaseController {
     @RestResult
     public List<OrderWorkerDTO> queryOrderWorkers(Long orderId) {
         return this.orderDomainService.queryOrderWorkers(orderId);
+    }
+
+    @GetMapping("/selectRoleEmployee")
+    @RestResult
+    public List<SimpleEmployeeDTO> selectRoleEmployee(Long roleId) {
+        UserContext userContext = WebContextUtils.getUserContext();
+        Long orgId = userContext.getOrgId();
+        return this.employeeService.querySimpleEmployeeInfo(orgId, roleId);
     }
 }
