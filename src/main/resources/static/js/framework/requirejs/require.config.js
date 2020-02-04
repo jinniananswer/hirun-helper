bbd2e7a5087212bea2d24f25ee56c7a5d2308b55
jinniananswer <38103152@qq.com>
2020-02-05 01:50:27 +0800

    【新增】新增根据角色选择员工组件

--- src/main/resources/static/js/framework/requirejs/require.config.js
+++ src/main/resources/static/js/framework/requirejs/require.config.js
@@ -16,7 +16,8 @@ require.config({
         'util': 'static/js/framework/extend/util',
         'cust-info': 'static/js/modules/bss/component/cust.info',
         'order-info': 'static/js/modules/bss/component/order.info',
-        'order-worker': 'static/js/modules/bss/component/order.worker'
+        'order-worker': 'static/js/modules/bss/component/order.worker',
+        'order-selectemployee': 'static/js/modules/bss/component/order.selectemployee'
     },
     shim: {
         'ELEMENT': {
