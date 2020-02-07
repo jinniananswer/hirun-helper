27037578b017f387b412cff8464f755c566a52b5
wantongsun <441973370@qq.com>
2020-02-07 20:36:54 +0800

    【新增】order_fee与order_paymoney处理方法

---
+++ src/main/java/com/microtomato/hirun/modules/bss/order/service/IOrderPaymoneyService.java
@@ -0,0 +1,16 @@
+package com.microtomato.hirun.modules.bss.order.service;
+
+import com.microtomato.hirun.modules.bss.order.entity.po.OrderPaymoney;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 付款类型明细表 服务类
+ * </p>
+ *
+ * @author sunxin
+ * @since 2020-02-05
+ */
+public interface IOrderPaymoneyService extends IService<OrderPaymoney> {
+
+}
