0e0b23f4998774d31c374aa9840bea7ec27d0bac
jinniananswer <38103152@qq.com>
2020-02-10 15:54:34 +0800

    【修改】添加操作码

--- src/main/java/com/microtomato/hirun/modules/bss/order/entity/consts/OrderConst.java
+++ src/main/java/com/microtomato/hirun/modules/bss/order/entity/consts/OrderConst.java
@@ -24,12 +24,17 @@ public class OrderConst {
     public static String OPER_LOG_CONTENT_STATUS_CHANGE = "订单状态变化";
 
     /**
-     * 订单状态往下一状态流转操作码
+     * 订单操作往下一状态流转操作码
      */
     public static String OPER_NEXT_STEP = "NEXT";
 
     /**
-     * 订单状态审核不通过
+     * 订单操作审核不通过
      */
     public static String OPER_AUDIT_NO = "AUDIT_NO";
+
+    /**
+     * 订单操作延迟
+     */
+    public static String OPER_DELAY = "DELAY";
 }
