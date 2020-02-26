8805974aa15af73024f9b7e62393ee140a9f112d
jinniananswer <38103152@qq.com>
2020-02-27 00:22:54 +0800

    【修改】调整默认的成功提示，改为默认关闭页面

--- src/main/resources/static/js/framework/extend/ajax.js
+++ src/main/resources/static/js/framework/extend/ajax.js
@@ -1,29 +1,23 @@
 define(['vue','ELEMENT','axios', 'qs'], function(Vue,element,axios, QS){
     let vm = new Vue({});
     let ajax = {
-        get: function(url, request, successFunc, errFunc, needLoad) {
-            this.execute('get', url, request, successFunc, errFunc, needLoad);
+        get: function(url, request, successFunc, errFunc, needConfirm) {
+            this.execute('get', url, request, successFunc, errFunc, needConfirm);
         },
 
-        post: function(url, request, successFunc, errFunc, needLoad) {
-            this.execute('post', url, request, successFunc, errFunc, needLoad);
+        post: function(url, request, successFunc, errFunc, needConfirm) {
+            this.execute('post', url, request, successFunc, errFunc, needConfirm);
         },
 
-        execute: function(methodType, url, request, successFunc, errFunc, needLoad) {
-            if (needLoad == null || typeof(needLoad) == "undefined") {
-                needLoad = true;
-            }
+        execute: function(methodType, url, request, successFunc, errFunc, needConfirm) {
 
-            let loading = null;
+            let loading = vm.$loading({
+                lock: true,
+                text: '拼命加载中……',
+                spinner: 'el-icon-loading',
+                background: 'rgba(0, 0, 0, 0.7)'
+            });
 
-            if (needLoad) {
-                loading = vm.$loading({
-                    lock: true,
-                    text: '拼命加载中……',
-                    spinner: 'el-icon-loading',
-                    background: 'rgba(0, 0, 0, 0.7)'
-                });
-            }
             let successMethod = function(info) {
                 let data = info.data;
                 let code = data.code;
@@ -38,27 +32,37 @@ define(['vue','ELEMENT','axios', 'qs'], function(Vue,element,axios, QS){
 
                     });
                 } else if (successFunc == null || typeof(successFunc) == "undefined") {
-                    vm.$confirm('点击确定按钮刷新本页面，点击关闭按钮关闭本界面', '操作成功', {
-                        confirmButtonText: '确定',
-                        type: 'success',
-                        center: true
-                    }).then(() => {
-                        document.location.reload();
-                    }).catch(() => {
-                        top.layui.admin.closeThisTabs();
-                    });
+                    if (needConfirm) {
+                        vm.$confirm('点击确定按钮刷新本页面，点击关闭按钮关闭本界面', '操作成功', {
+                            confirmButtonText: '确定',
+                            type: 'success',
+                            center: true
+                        }).then(() => {
+                            document.location.reload();
+                        }).catch(() => {
+                            top.layui.admin.closeThisTabs();
+                        });
+                    } else {
+                        vm.$alert('点击确定关闭本页面', '操作成功', {
+                            confirmButtonText: '确定',
+                            callback: action => {
+                                top.layui.admin.closeThisTabs();
+                            }
+                        });
+                    }
+
                 } else {
                     successFunc(data.rows);
                 }
 
-                if (needLoad && loading != null) {
+                if (loading != null) {
                     loading.close();
                 }
             }
 
             if (errFunc == null || typeof(errFunc) == "undefined") {
                 errFunc = function(data) {
-                    if (needLoad && loading != null) {
+                    if (loading != null) {
                         loading.close();
                     }
                     vm.$message.error('对不起，我们的系统出错了，请赶紧联系管理员报告错误吧，'+data.statusText+'我们将第一时间解决您的问题');
