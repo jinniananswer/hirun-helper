5af05894d85a3dbbc277c5bc1df507aae1c5ac12
jinniananswer <38103152@qq.com>
2020-02-26 22:51:46 +0800

    【修改】去掉暂时用不到的方法

--- src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderFeeController.java
+++ src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderFeeController.java
@@ -1,19 +1,15 @@
 package com.microtomato.hirun.modules.bss.order.controller;
 
 import com.microtomato.hirun.framework.annotation.RestResult;
-import com.microtomato.hirun.modules.bss.customer.entity.dto.CustConsultDTO;
-import com.microtomato.hirun.modules.bss.customer.entity.dto.CustPreparationDTO;
 import com.microtomato.hirun.modules.bss.order.entity.dto.OrderFeeDTO;
-import com.microtomato.hirun.modules.bss.order.entity.po.OrderConsult;
 import com.microtomato.hirun.modules.bss.order.entity.po.OrderFee;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import lombok.extern.slf4j.Slf4j;
-
 import com.microtomato.hirun.modules.bss.order.service.IOrderFeeService;
-
-import java.util.List;
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -31,65 +27,21 @@ public class OrderFeeController {
     @Autowired
     private IOrderFeeService orderFeeServiceImpl;
 
-    @PostMapping("/addDesignFee")
-    @RestResult
-    public void addDesignFee(@RequestBody OrderFeeDTO orderFee) {
-        System.out.println("orderFee=========="+orderFee.toString());
-        orderFee.setOrderId((long)7);//测试用
-        orderFeeServiceImpl.addDesignFee(orderFee);
-    }
-
-    @PostMapping("/addDownPayment")
-    @RestResult
-    public void addDownPayment(@RequestBody OrderFeeDTO orderFee) {
-        log.debug(orderFee.toString());
-        orderFee.setOrderId((long)7);//测试用
-        orderFeeServiceImpl.addDownPayment(orderFee);
-    }
-
-    @PostMapping("/loadDesignFeeInfo")
-    @RestResult
-    public OrderFeeDTO loadDesignFeeInfo(Long orderId){
-        System.out.print("orderId=====rrrrrrrrr===="+orderId);
-        return orderFeeServiceImpl.loadDesignFeeInfo(orderId);
-    }
-
-    @PostMapping("/loadProjectFeeInfo")
-    @RestResult
-    public OrderFeeDTO loadProjectFeeInfo(Long orderId,String period){
-        System.out.print("orderId=====rrrrrrrrr===="+orderId);
-        System.out.print("period=====rrrrrrrrr===="+period);
-        return orderFeeServiceImpl.loadDesignFeeInfo(orderId);
-    }
-
-    @PostMapping("/auditUpdateForDesign")
-    @RestResult
-    public void auditUpdateForDesign(OrderFeeDTO orderFee){
-        System.out.print("orderFee=====gggggg===="+orderFee);
-        orderFeeServiceImpl.auditUpdateForTotal(orderFee);
-    }
-
-    @PostMapping("/auditUpdateForProject")
-    @RestResult
-    public void auditUpdateForProject(OrderFeeDTO orderFee){
-        System.out.print("orderFee=====gggggg===="+orderFee);
-        orderFeeServiceImpl.auditUpdateForTotal(orderFee);
-    }
-
-//    @PostMapping("/auditUpdate")
-//    @RestResult
-//    public void auditUpdate(OrderFeeDTO orderFee){
-//        System.out.print("orderFee=====gggggg===="+orderFee);
-//        orderFeeServiceImpl.auditUpdate(orderFee);
-//    }
-
-//初始化方法
+    /**
+     *
+     * @param orderId
+     * @return
+     */
     @GetMapping("/queryOrderConsult")
     @RestResult
     public OrderFee queryOrderCollectFee(Long orderId){
         return orderFeeServiceImpl.queryOrderCollectFee(orderId);
     }
-//费用审核
+
+    /**
+     * 费用审核
+     * @param dto
+     */
     @PostMapping("/submitAudit")
     @RestResult
     public void submitAudit(OrderFeeDTO dto) {
