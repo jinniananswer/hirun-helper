e547e9c487311b4e4909dcd0149b263419443cba
liuhui <lh936679052@163.com>
2020-03-05 00:19:34 +0800

    【新增】新增驻点设计师上传图纸

---
+++ src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderGarrisonDesignController.java
@@ -0,0 +1,46 @@
+package com.microtomato.hirun.modules.bss.order.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.microtomato.hirun.framework.annotation.RestResult;
+import com.microtomato.hirun.modules.bss.order.entity.po.OrderGarrisonDesign;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+
+import com.microtomato.hirun.modules.bss.order.service.IOrderGarrisonDesignService;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author liuhui
+ * @since 2020-03-04
+ */
+@RestController
+@Slf4j
+@RequestMapping("/api/bss.order/order-garrison-design")
+public class OrderGarrisonDesignController {
+
+    @Autowired
+    private IOrderGarrisonDesignService orderGarrisonDesignServiceImpl;
+
+    @GetMapping("queryGarrisonDesignInfo")
+    @RestResult
+    public OrderGarrisonDesign queryGarrisonDesignInfo(Long orderId){
+        return orderGarrisonDesignServiceImpl.getOne(new QueryWrapper<OrderGarrisonDesign>().lambda()
+                .eq(OrderGarrisonDesign::getOrderId,orderId));
+    }
+
+    @PostMapping("saveGarrisonDesignInfo")
+    @RestResult
+    public void saveGarrisonDesignInfo(OrderGarrisonDesign orderGarrisonDesign){
+        orderGarrisonDesignServiceImpl.saveGarrisonDesignInfo(orderGarrisonDesign);
+    }
+
+    @PostMapping("submitBudget")
+    @RestResult
+    public void submitBudget(OrderGarrisonDesign orderGarrisonDesign){
+        orderGarrisonDesignServiceImpl.submitBudget(orderGarrisonDesign);
+    }
+}
