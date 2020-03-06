8650032eff1bfd30c6ade530886e0bca74be0797
liuhui <lh936679052@163.com>
2020-03-06 11:39:47 +0800

    【新增】新增尾款收取

--- src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderFeeController.java
+++ src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderFeeController.java
@@ -2,17 +2,16 @@ package com.microtomato.hirun.modules.bss.order.controller;
 
 import com.microtomato.hirun.framework.annotation.RestResult;
 import com.microtomato.hirun.modules.bss.config.entity.dto.PayComponentDTO;
+import com.microtomato.hirun.modules.bss.order.entity.dto.LastInstallmentCollectionDTO;
 import com.microtomato.hirun.modules.bss.order.entity.dto.OrderFeeDTO;
 import com.microtomato.hirun.modules.bss.order.entity.dto.SecondInstallmentCollectionDTO;
 import com.microtomato.hirun.modules.bss.order.entity.po.OrderFee;
 import com.microtomato.hirun.modules.bss.order.entity.po.OrderPayNo;
+import com.microtomato.hirun.modules.bss.order.service.IInstallmentCollectDomainService;
 import com.microtomato.hirun.modules.bss.order.service.IOrderFeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -29,6 +28,9 @@ public class OrderFeeController {
 
     @Autowired
     private IOrderFeeService orderFeeServiceImpl;
+
+    @Autowired
+    private IInstallmentCollectDomainService collectDomainService;
 
     /**
      *
@@ -75,6 +77,18 @@ public class OrderFeeController {
     @RestResult
     public void secondInstallmentCollect(SecondInstallmentCollectionDTO dto) {
         orderFeeServiceImpl.secondInstallmentCollect(dto);
+    }
+
+    @PostMapping("/saveLastInstallmentCollect")
+    @RestResult
+    public void saveLastInstallmentCollect(@RequestBody LastInstallmentCollectionDTO lastInstallmentCollectionDTO) {
+        collectDomainService.saveLastCollectionFee(lastInstallmentCollectionDTO);
+    }
+
+    @GetMapping("/queryLastInstallmentCollect")
+    @RestResult
+    public LastInstallmentCollectionDTO queryLastInstallmentCollect(Long orderId) {
+        return collectDomainService.queryLastInstallmentCollect(orderId);
     }
 
 }
---
+++ src/main/resources/static/js/modules/bss/order/last.installment.collection.js
@@ -0,0 +1,122 @@
+require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'order-info', 'order-worker', 'order-selectemployee','cust-visit', 'order-search-employee'], function(Vue, element, axios, ajax, vueselect, util, custInfo, orderInfo, orderWorker, orderSelectEmployee,custVisit, orderSearchEmployee) {
+    let vm = new Vue({
+        el: '#app',
+        data() {
+            return {
+                lastInstallment: {
+                    orderId:'30',
+                    chargedAllFee:'0',
+                    chargedLastFee:'0',
+                    chargedMaterialFee:'0',
+                    chargedCupboardFee:'0',
+                    woodProductFee:'',
+                    baseDecorationFee:'',
+                    doorFee:'0',
+                    taxFee:'0',
+                    furnitureFee:'0',
+                    otherFee:'0',
+                },
+                workerSalary: {
+                    orderId:'30',
+                    hydropowerSalary: '',
+                    hydropowerRemark: '',
+                    woodworkerSalary: '',
+                    woodworkerRemark:'',
+                    tilerSalary: '',
+                    tilerRemark: '',
+                    painterSalary:'',
+                    painterRemark:'',
+                    wallworkerSalary:'',
+                    wallworkerRemark:'',
+                },
+                //orderId : util.getRequest('orderId'),
+                //orderStatus : util.getRequest('status'),
+                lastInstallmentRules : {
+                    doorFee: [
+                        {required: true, message: '请填写门总金额', trigger: 'blur'},
+                    ],
+                    furnitureFee: [
+                        {required: true, message: '请填写家具总金额', trigger: 'blur'},
+                    ],
+                },
+            }
+        },
+        computed: {
+            getWoodProductFee: {
+                get() {
+                    let woodProductFee = parseFloat(this.lastInstallment.doorFee) + parseFloat(this.lastInstallment.furnitureFee)
+                    this.lastInstallment.woodProductFee = woodProductFee;
+                    return woodProductFee;
+                }
+            },
+            getBaseDecorationFee : {
+                get() {
+                    let baseDecorationFee = parseFloat(this.lastInstallment.chargedAllFee)-parseFloat(this.lastInstallment.woodProductFee)
+                        -parseFloat(this.lastInstallment.taxFee)-parseFloat(this.lastInstallment.otherFee);
+                    this.lastInstallment.baseDecorationFee = baseDecorationFee;
+                    return baseDecorationFee;
+                }
+            },
+        },
+
+        mounted: function() {
+            this.init();
+        },
+        methods: {
+
+            init:function(){
+                let that = this;
+                ajax.get('api/bss/order/order-fee/queryLastInstallmentCollect', {orderId:this.lastInstallment.orderId}, function(data) {
+                    Object.assign(that.workerSalary, data.workerSalaryDTO);
+                    Object.assign(that.lastInstallment, data.lastInstallmentInfoDTO);
+                })
+            },
+
+            save : function() {
+                axios({
+                    method: 'post',
+                    url: 'api/bss/order/order-fee/saveLastInstallmentCollect',
+                    data: {
+                        "lastInstallmentInfoDTO": this.lastInstallment,
+                        "workerSalaryDTO": this.workerSalary,
+                    }
+                }).then(function (responseData) {
+                    if (0 == responseData.data.code) {
+                        vm.$confirm('点击确定按钮刷新本页面，点击关闭按钮关闭本界面', '操作成功', {
+                            confirmButtonText: '确定',
+                            type: 'success',
+                            center: true
+                        }).then(() => {
+                            document.location.reload();
+                        }).catch(() => {
+                            top.layui.admin.closeThisTabs();
+                        });
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                    Vue.prototype.$message({
+                        message: '保存失败！',
+                        type: 'warning'
+                    });
+                });
+
+/*                this.$refs['lastInstallment'].validate((valid) => {
+                    if (valid) {
+                        let url = 'api/bss/order/order-fee/saveLastInstallmentCollect';
+                        let data = {
+                            "lastInstallmentInfoDTO": this.lastInstallment,
+                            "workerSalaryDTO": this.workerSalary,
+                        };
+                        ajax.post(url, data);
+                    } else {
+                        return false;
+                    }
+                });*/
+            },
+
+        }
+    });
+
+    return vm;
+
+})
