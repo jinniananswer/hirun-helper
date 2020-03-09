c40050de2b21bbd75e168200a4fada78a17564fb
xiaocl <123456>
2020-03-09 23:31:03 +0800

    add by xiaocl@

--- src/main/resources/static/js/modules/bss/order/cust.orderaudit.js
+++ src/main/resources/static/js/modules/bss/order/cust.orderaudit.js
@@ -23,9 +23,9 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
 
         mounted: function() {
             let data = {
-                id : util.getRequest('id'),
+                orderId : util.getRequest('orderId'),
             }
-            alert(JSON.stringify(data));
+            //alert(JSON.stringify(data));
             ajax.post('api/bss.order/order-wholeRoomDrawing/getWholeRoomDraw', data, (responseData)=>{
                 Object.assign(this.wholeRoomDrawing, responseData);
             });
