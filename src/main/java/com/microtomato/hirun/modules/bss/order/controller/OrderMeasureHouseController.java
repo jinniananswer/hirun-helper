90c4a03ca3f45d62706fdf27aba41d66c88df458
xiaocl <123456>
2020-03-09 23:57:12 +0800

    add by xiaocl@

--- src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderMeasureHouseController.java
+++ src/main/java/com/microtomato/hirun/modules/bss/order/controller/OrderMeasureHouseController.java
@@ -1,17 +1,13 @@
 package com.microtomato.hirun.modules.bss.order.controller;
 
 import com.microtomato.hirun.framework.annotation.RestResult;
-import com.microtomato.hirun.modules.bss.customer.entity.dto.CustConsultDTO;
-import com.microtomato.hirun.modules.bss.order.entity.consts.OrderConst;
 import com.microtomato.hirun.modules.bss.order.entity.po.OrderMeasureHouse;
+import com.microtomato.hirun.modules.bss.order.entity.po.OrderPlaneSketch;
 import com.microtomato.hirun.modules.bss.order.service.IOrderMeasureHouseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ：xiaocl
@@ -53,5 +49,11 @@ public class OrderMeasureHouseController {
     @RestResult
     public void submitToMeasureSuspendFlow(@RequestBody OrderMeasureHouse orderMeasureHouse) {
         orderMeasureHouseServiceImpl.submitToMeasureSuspendFlow(orderMeasureHouse.getOrderId());
+    }
+
+    @GetMapping("/getMeasureHouse")
+    @RestResult
+    public OrderMeasureHouse getMeasureHouse(Long orderId) {
+        return orderMeasureHouseServiceImpl.getMeasureHouse(orderId);
     }
 }
