ab33db9138005d6e8ef446dffecbc6d6decd09c4
xiaocl <123456>
2020-03-10 20:32:16 +0800

    add by xiaocl@

--- src/main/resources/static/js/modules/bss/order/order.planesketch.js
+++ src/main/resources/static/js/modules/bss/order/order.planesketch.js
@@ -39,8 +39,8 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
                 }
                 ajax.get('api/bss.order/order-planSketch/getPlaneSketch', data, (responseData)=>{
                     Object.assign(this.planFigureInfos, responseData);
-                 });
-                //alert(JSON.stringify(this.planFigureInfos));
+                });
+                alert(JSON.stringify(this.planFigureInfos));
                 if (this.orderStatus=='35') {
                     this.isBackToDesigner = true;
                 }
@@ -91,6 +91,10 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
                     });
                     return false;
                 }
+                ajax.get('api/bss.order/order-planSketch/getPlaneSketch', data, (responseData)=>{
+                    Object.assign(this.planFigureInfos, responseData);
+                });
+
                 ajax.post('api/bss.order/order-planSketch/submitPlaneSketch', this.planFigureInfos,null,null,true);
             },
             //修改平面图时间
@@ -110,7 +114,7 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
                     });
                     return false;
                 }
-                let url = 'api/bss.order/order-file/view/' + util.getRequest("orderId") + "/456";
+                /*let url = 'api/bss.order/order-file/view/' + util.getRequest("orderId") + "/456";
                 let rn = false;
                 ajax.get(url, null,function(data){
                     if (data == null) {
@@ -122,9 +126,12 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
                         return;
                     }
 
-                });
+                });*/
                 this.planFigureInfos.employeeId = this.eid;
-                ajax.post('api/bss.order/order-planSketch/submitToSignContractFlow', this.planFigureInfos,null,null,true);
+                ajax.post('api/bss.order/order-planSketch/submitToSignContractFlow', this.planFigureInfos,(responseData)=>{
+
+                });
+
                 ajax.get('api/bss.order/order-planSketch/updateOrderWork', {
                     orderId : this.planFigureInfos.orderId,
                     roleId : '34',
@@ -140,7 +147,7 @@ require(['vue', 'ELEMENT', 'axios', 'ajax', 'vueselect', 'util','cust-info', 'or
             },
             backOrderWork : function () {
                 //回写订单状态
-                ajax.post('api/bss.order/order-planSketch/updateOrderWork', {
+                ajax.get('api/bss.order/order-planSketch/updateOrderWork', {
                     orderId : this.planFigureInfos.orderId,
                     roleId : '34',
                     employeeId : this.eid,
