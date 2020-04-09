6509bd830c41e2e4689faa41489670ff7dda0253
liuhui <lh936679052@163.com>
2020-04-09 17:43:19 +0800

    【新增】集成elementui 个性化组件

---
+++ src/main/resources/static/js/modules/bss/order/component/cust.info.js
@@ -0,0 +1,86 @@
+define(['vue','ELEMENT','ajax'], function(Vue,element,ajax){
+    Vue.component('cust-info', {
+        props: ['cust-id', 'order-id'],
+
+        data : function(){
+            return {
+                sCustId: this.custId,
+                sOrderId: this.orderId,
+                customer:{}
+            }
+        },
+
+        template: `
+            <el-card class="box-card" shadow="hover">
+                <div slot="header" class="clearfix">
+                    <span>客户信息</span>
+                </div>
+                <el-row :gutter="12">
+                    <el-col :span="8">
+                        <el-avatar :size="60" v-if="customer.headUrl != null" :src="customer.headUrl"></el-avatar>
+                        <el-avatar :size="60" v-else>{{customer.custName!=null?customer.custName.substring(0,1):''}}</el-avatar>
+                    </el-col>
+                    <el-col :span="16">
+                        <h3 v-if="customer.custName != null && customer.custName != ''">{{customer.custName}}</h3>
+                        <h3 v-else>未录入</h3>
+                    </el-col>
+                </el-row>
+                <el-divider>基本资料</el-divider>
+                <div class="text item">
+                    客户编码：<template v-if="customer.custNo != null && customer.custNo != ''"><el-tag>{{customer.custNo}}</el-tag></template><template v-else>未录入</template>
+                </div>
+                <div class="text item">
+                    性别：<template v-if="customer.sexName != null && customer.sexName != ''"><el-tag>{{customer.sexName}}</el-tag></template><template v-else>未录入</template>
+                </div>
+                <div class="text item">
+                    电话：<template v-if="customer.mobileNo != null && customer.mobileNo != ''"><el-tag>{{customer.mobileNo}}</el-tag></template><template v-else>未录入</template>
+                </div>
+                <el-collapse accordion>
+                    <el-collapse-item title="更多信息" name="1">
+                        <div class="item">年龄：<template v-if="customer.age != null && customer.age != ''"><el-tag>{{customer.age}}</el-tag></template><template v-else>未录入</template></div>
+                        <div class="item">学历：<template v-if="customer.educational != null && customer.educational != ''"><el-tag>{{customer.educational}}</el-tag></template><template v-else>未录入</template></div>
+                        <div class="item">家人数量：<template v-if="customer.familyMembersCount != null && customer.familyMembersCount != ''"><el-tag>{{customer.familyMembersCount}}</el-tag></template><template v-else>未录入</template></div>
+                        <div>客户兴趣爱好：<template v-if="customer.hobby != null && customer.hobby != ''"><el-tag type="warning">{{customer.hobby}}</el-tag></template><template v-else>未录入</template></div>
+                    </el-collapse-item>
+                </el-collapse>
+            </el-card>
+            `,
+
+        methods: {
+            init() {
+                let that = this;
+                ajax.get('api/customer/cust-base/getCustInfo', {custId:this.sCustId, orderId:this.sOrderId}, function(data) {
+                    that.customer = data;
+                })
+            }
+        },
+
+        watch: {
+            custId(val) {
+                this.sCustId = val;
+            },
+
+            sCustId(val, oldValue) {
+                if (val != oldValue) {
+                    this.init();
+                }
+            },
+
+            orderId(val) {
+                this.sOrderId = val;
+            },
+
+            sCustId(val, oldValue) {
+                if (val != oldValue) {
+                    this.init();
+                }
+            }
+        },
+
+        mounted () {
+            this.init();
+        }
+    });
+
+
+})
---
+++ src/main/resources/static/js/modules/bss/order/component/order.file.upload.js
@@ -0,0 +1,114 @@
+define(['vue', 'ELEMENT', 'ajax'], function (Vue, element, ajax) {
+    Vue.component('order-file-upload', {
+        props: {
+            buttonName: {default: '上传文件'},
+            accept: {required: true},
+            limit: {
+                type: Number,
+                default: 1
+            },
+            orderId: {
+                required: true,
+                type: Number,
+            },
+            stage: {
+                required: true,
+                type: Number,
+            },
+        },
+        template: `
+<div>
+<el-upload
+    class="order-file-upload"
+    action="api/bss.order/order-file/uploadOne"
+    :data="data"
+    :accept="accept"
+    :limit="limit"
+    :file-list="fileList"
+    :before-upload="handleBefore"
+    :on-exceed="handleExceed"
+    :on-success="handleSuccess"
+    :on-remove="handleRemove">
+<el-button type="primary">{{buttonName}}</el-button>
+<el-tag>文件类型: {{accept}}</el-tag>
+</el-upload>
+</div>
+`,
+        data: function () {
+            return {
+                limit: this.limit,
+                accept: this.accept,
+                data: {
+                    orderId: this.orderId,
+                    stage: this.stage
+                },
+                fileList: [],
+            }
+        },
+
+        computed: {
+
+            fileId: function () {
+                let fileId = '';
+                this.fileList.forEach(function (e, index, array) {
+                    fileId += e.response
+                    if (index < array.length - 1) {
+                        fileId += ','
+                    }
+                })
+                return fileId;
+
+            }
+        },
+
+        methods: {
+            handleBefore(file) {
+                let filename = file.name
+                let arr = filename.split('.')
+                if (this.accept.indexOf(arr[1]) == -1) {
+                    this.$message({
+                        type: 'warning',
+                        duration: 0,
+                        showClose: true,
+                        message: '只能上传格式为 ' + this.accept + ' 的文件!'
+                    });
+                    return false
+                }
+            },
+            handleExceed(files, fileList) {
+                this.$message({
+                    type: 'warning',
+                    duration: 0,
+                    showClose: true,
+                    message: '只能传 ' + this.limit + ' 个文件！'
+                });
+                return false
+            },
+            handleSuccess(response, file, fileList) {
+                this.$message({
+                    type: 'success',
+                    message: '【' + file.name + '】上传成功！'
+                });
+            },
+            handleRemove(file, fileList) {
+                this.fileList = fileList.slice(-this.limit);
+                this.$message({
+                    type: 'success',
+                    message: '【' + file.name + '】删除成功！'
+                });
+            },
+        },
+
+        watch: {
+            stage(val) {
+                this.data.stage = val;
+            },
+        },
+
+        mounted() {
+
+        }
+    })
+})
+
+
