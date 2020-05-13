15a8e6acc4cbf3392566c15c1e7aead4c0b7889a
jinniananswer <38103152@qq.com>
2020-05-14 00:25:55 +0800

    【修改】新增员工月固定工资编辑与审核功能

---
+++ src/main/resources/static/js/modules/organization/salary/edit.salary.fix.js
@@ -0,0 +1,51 @@
+require(['vue','ELEMENT','ajax', 'vxe-table', 'vueselect', 'org-orgtree', 'util'], function(Vue, element, ajax, table, vueSelect, orgTree, util) {
+    Vue.use(table);
+    let vm = new Vue({
+        el: '#app',
+        data: function() {
+            return {
+                queryCond: {
+                    page : 1,
+                    limit : -1
+                },
+                employees: []
+            }
+        },
+
+        methods: {
+            query: function() {
+                let that = this;
+                ajax.get('api/organization/employee-salary-fix/queryFixSalary', this.queryCond, function(data){
+                    that.employees = data;
+                });
+            },
+
+            submit : function() {
+                ajax.post('api/organization/employee-salary-fix/submitFixSalaries', this.employees);
+            },
+
+            audit : function() {
+                ajax.post('api/organization/employee-salary-fix/auditFixSalaries', this.employees);
+            },
+
+            isModify: function(obj) {
+                if (obj.row.isModified == '1') {
+                    return "modify_row";
+                }
+            },
+
+            activeRowMethod ({ row, rowIndex }) {
+                if (row.auditStatus ==  '1' || row.auditStatus == '2' || row.auditStatus == '4') {
+                    return false;
+                }
+
+                return true;
+            }
+        },
+
+        mounted () {
+        }
+    });
+
+    return vm;
+});
