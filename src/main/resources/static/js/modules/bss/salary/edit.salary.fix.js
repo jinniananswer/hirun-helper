577184b946b3070abf7c9b616222085341fdef86
jinniananswer <38103152@qq.com>
2020-08-29 23:47:18 +0800

    【修改】修改bug

--- src/main/resources/static/js/modules/bss/salary/edit.salary.fix.js
+++ src/main/resources/static/js/modules/bss/salary/edit.salary.fix.js
@@ -15,7 +15,7 @@ require(['vue','ELEMENT','ajax', 'vxe-table', 'vueselect', 'org-orgtree', 'util'
         methods: {
             query: function() {
                 let that = this;
-                ajax.get('api/bss.salary/salary-fix/queryFixSalary', this.queryCond, function(data){
+                ajax.post('api/bss.salary/salary-fix/queryFixSalary', this.queryCond, function(data){
                     that.employees = data;
                 });
             },
