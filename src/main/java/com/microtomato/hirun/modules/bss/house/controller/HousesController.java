e69a7f0eb0067fbe6df13920d3bbf5a2003dd71c
jinniananswer <38103152@qq.com>
2020-10-18 21:52:34 +0800

    【修改】调整楼盘选择组件

--- src/main/java/com/microtomato/hirun/modules/bss/house/controller/HousesController.java
+++ src/main/java/com/microtomato/hirun/modules/bss/house/controller/HousesController.java
@@ -1,17 +1,13 @@
 package com.microtomato.hirun.modules.bss.house.controller;
 
 import com.microtomato.hirun.framework.annotation.RestResult;
-import com.microtomato.hirun.framework.util.ArrayUtils;
 import com.microtomato.hirun.modules.bss.house.entity.po.Houses;
-import com.microtomato.hirun.modules.system.service.IStaticDataService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import lombok.extern.slf4j.Slf4j;
-
 import com.microtomato.hirun.modules.bss.house.service.IHousesService;
-
+import com.microtomato.hirun.modules.system.service.IStaticDataService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -38,14 +34,7 @@ public class HousesController {
     @GetMapping("queryHouse")
     @RestResult
     public List<Houses> queryHouse(){
-        List<Houses> housesList=housesServiceImpl.list();
-        if(ArrayUtils.isNotEmpty(housesList)){
-            for(Houses houses:housesList){
-                String natureName=staticDataService.getCodeName("HOUSE_NATURE",houses.getNature()+"");
-                houses.setName(houses.getName()+"【"+natureName+"】");
-            }
-        }
-        return housesList;
+        return this.housesServiceImpl.queryAll();
     }
 
 }
